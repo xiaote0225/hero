69e43045fda3ad6efae6c07d6d623dee9bb95162
wangxun <wangxun0225@163.com>
2021-05-05 23:04:16 +0800

    feat:内联模板：

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { Comp3Component } from './comp3/comp3.component';
 import { Comp4Component } from './comp4/comp4.component';
 import { Comp5Component } from './comp5/comp5.component';
 import { Comp6Component } from './comp6/comp6.component';
+import { Comp7Component } from './comp7/comp7.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { Comp6Component } from './comp6/comp6.component';
     Comp3Component,
     Comp4Component,
     Comp5Component,
-    Comp6Component
+    Comp6Component,
+    Comp7Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/comp7/comp7.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp7',
+  template: `
+    <p>
+      comp7 works!
+    </p>
+  `,
+  styleUrls: ['./comp7.component.scss']
+})
+export class Comp7Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
