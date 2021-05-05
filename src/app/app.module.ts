5b5599abe0086d8795bfbf5324614ecc05490cbe
wangxun <wangxun0225@163.com>
2021-05-05 22:55:45 +0800

    feat:在新组件中使用的变更检测策略;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Comp1Component
+    Comp1Component,
+    Comp2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/comp2/comp2.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-comp2',
+  templateUrl: './comp2.component.html',
+  styleUrls: ['./comp2.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Comp2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
