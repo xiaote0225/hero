c4f87c848f2a46499add0134f340fb98f41a8f23
wangxun <wangxun0225@163.com>
2021-05-05 22:58:43 +0800

    feat:将从声明模块中导出此组件;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,24 @@ import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2Component } from './comp2/comp2.component';
 import { Comp3Component } from './comp3/comp3.component';
+import { Comp4Component } from './comp4/comp4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Comp1Component,
     Comp2Component,
-    Comp3Component
+    Comp3Component,
+    Comp4Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    Comp4Component
+  ]
 })
 export class AppModule { }
