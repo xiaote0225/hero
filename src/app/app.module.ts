0bbafd03f0a7d69d3190a5253150ff073541441f
wangxun <wangxun0225@163.com>
2021-05-05 23:03:29 +0800

    feat:内联yangshi;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { Comp2Component } from './comp2/comp2.component';
 import { Comp3Component } from './comp3/comp3.component';
 import { Comp4Component } from './comp4/comp4.component';
 import { Comp5Component } from './comp5/comp5.component';
+import { Comp6Component } from './comp6/comp6.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { Comp5Component } from './comp5/comp5.component';
     Comp2Component,
     Comp3Component,
     Comp4Component,
-    Comp5Component
+    Comp5Component,
+    Comp6Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/comp6/comp6.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp6',
+  templateUrl: './comp6.component.html',
+  styles: [
+  ]
+})
+export class Comp6Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
