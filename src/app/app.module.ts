f5bf45eeb9d24f8666bb98c429906ba34c56cb66
wangxun <wangxun0225@163.com>
2021-05-05 22:51:16 +0800

    feat:创建comp1组件;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component
   ],
   imports: [
     BrowserModule,
