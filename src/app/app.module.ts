79a2bedae08e2aedb4bea83c1babd26796a0b6bf
wangxun <wangxun0225@163.com>
2021-05-05 22:57:30 +0800

    feat:指定在是否要在样式中包含:host{display:block;};

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2Component } from './comp2/comp2.component';
+import { Comp3Component } from './comp3/comp3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Comp1Component,
-    Comp2Component
+    Comp2Component,
+    Comp3Component
   ],
   imports: [
     BrowserModule,
