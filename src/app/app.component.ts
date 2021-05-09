b1b2ca9112c9197280aa4d4e23cbc99d4c318e1c
wangxun <wangxun0225@163.com>
2021-05-09 12:21:25 +0800

    feat:事件对象;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
   onClick(){
     console.log('onClick');
   }
+  onClick1(event: MouseEvent){
+    console.log('event',event);
+    console.log('event.target',event.target);
+  }
+  onInput(event: KeyboardEvent){
+    console.log('event',event);
+    console.log('event',event.target);
+    console.log('event',(event.target as HTMLInputElement).value);
+  }
+  clickParent(){
+    console.log('parent......');
+  }
+  clickChild(event:MouseEvent){
+    event.stopPropagation();
+    console.log('child......');
+  }
 }
